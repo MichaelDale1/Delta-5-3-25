db57cd2b991e43e4ece2274057c01c8fd8bb37b0
MichaelDale1 <king.konvoy1@gmail.com>
2025-05-03 17:27:26 -0400

    2nd upload

---
+++ 0-authentication.tf
@@ -0,0 +1,11 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs
+provider "google" {
+  project = "gcpworldboss3-11-25"
+  region  = "us-central1"
+  credentials = "gcpworldboss3-11-25-12ccb4a9a9a6.json"
+}
+
+# I'm doing this tonight while prepping my students for their sessions with Lizzo
+
+# comment there will be no diddy oil bootygasms here
+# Lizzo will be pegging Carlton and inducing a bootygasm deep in him with Chewbacca watching!
