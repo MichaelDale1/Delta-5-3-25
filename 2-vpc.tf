180822851ccd06074ab9c01c9050e9bac77cc407
MichaelDale1 <king.konvoy1@gmail.com>
2025-05-11 16:10:39 -0400

    added 2nd vpc during 5-10-25 lab

--- 2-vpc.tf
+++ 2-vpc.tf
@@ -2,7 +2,17 @@
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
 resource "google_compute_network" "summer-ready" {
-  name                            = "summer-readyain"
+  name                            = "summer-ready"
+  routing_mode                    = "REGIONAL"
+  auto_create_subnetworks         = false
+  mtu                             = 1460
+  delete_default_routes_on_create = false
+
+}
+
+
+resource "google_compute_network" "prod1" {
+  name                            = "prod1"
   routing_mode                    = "REGIONAL"
   auto_create_subnetworks         = false
   mtu                             = 1460
